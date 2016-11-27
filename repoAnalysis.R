c8188d43a91d3e30a3681b498344aa9e56656d88
jcarlos <carlosmaster10@gmail.com>
2016-11-27 01:15:06 -0300

    minor fixes

--- repoAnalysis.R
+++ repoAnalysis.R
@@ -1,25 +1,37 @@
 
-ag <- aggregate(repoAnalysis$V5, by= list(repoAnalysis$V1, repoAnalysis$V2, 
-                                    repoAnalysis$V4), FUN = sum)
-
-for (repository in unique(ag$Group.1)) {
-  a <-  ag[which(ag$Group.1 == repository), ]
-  dir.create(paste("output",repository,sep="/"), showWarnings = FALSE)
-  for (declaration in unique(a$Group.3)) {
-    b <- a[which(a$Group.3 == declaration), ]
-    b$Group.2 <- factor(b$Group.2)
+for (repository in unique(repoAnalysis$V1)) {
+  a <- repoAnalysis[which(repoAnalysis$V1 == repository),]
+  FOLDERPATH = paste("output",repository,sep="/")
+  dir.create(FOLDERPATH, showWarnings = FALSE)
+  for (declaration in unique(a$V4)) {
+    ag <- a[which(a$V4 == declaration),]
+    b <- aggregate(ag$V5, by=list(factor(ag$V2,levels=unique(ag$V2))), FUN=sum)
+    
+    b$Group.1 <- factor(b$Group.1)
+    
     if( max(b$x) < 5 ) { next }
-    WDT = length(b$x)/5
-    HGT = max(b$x)/5
-    if (WDT < 5) { WDT = 5 }
-    if (HGT < 3){ HGT = 3 }
-    png(paste("output", repository, paste(declaration,"graph.png", 
-              sep = "_"), sep = "/"), width=WDT, height=7, units="in",
-              res=200) ; par(mar=c(18, 1, 1, 1) + 1.1)
-    x <- barplot(b$x, xaxt = "n", main = paste(
-      paste("'", repository, "':", sep = ""), declaration))
-    labs <- paste(names(table(b$Group.2)))
-    text(cex=0.9, x=x+0.5, y=-0.5, labs, xpd=TRUE, srt=90, pos=2)
-    dev.off()
+    
+	WDT = length(b$x)/5
+	if (WDT < 5) { WDT = 5 }
+	
+	FILEPATH = paste(FOLDERPATH, paste(declaration,"graph.png", sep = "_"),
+					sep = "/")
+	
+	res <- try(png(FILEPATH, width=WDT, height=10, units="in", res=200))
+	
+	if(inherits(res, "try-error")){
+		next
+	}
+	
+	par(mar=c(18, 1, 1, 1) + 1.1)
+	
+	x <- barplot(b$x, xaxt = "n", main = paste(
+	  paste("'", repository, "':", sep = ""), declaration))
+	labs <- paste(names(table(b$Group.1)))
+	text(cex=0.9, x=x+0.5, y=-0.5, labs, xpd=TRUE, srt=90, pos=2)
+	
+	dev.off()
   }
+  
+  if(length(dir(FOLDERPATH)) == 0) { unlink(FOLDERPATH, recursive=TRUE, force=TRUE) }
 }
